01ea78a59fbf35705a8b0c515f1b95c169f2b0e5
GeneralRogov <alexanderogov@list.ru>
2021-04-21 00:18:20 +0300

    Spring Boot Добавил авторизацию, Spring Security

---
+++ springBoot/src/main/java/com/rog/teach/domain/User.java
@@ -0,0 +1,60 @@
+package com.rog.teach.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "User_$T")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String password;
+    private boolean active;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
