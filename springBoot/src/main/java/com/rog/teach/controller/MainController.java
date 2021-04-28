9883a088709a1c2fb17c2c0060e4aab7364760b7
GeneralRogov <alexanderogov@list.ru>
2021-04-28 21:23:59 +0300

    Загрузка файлов на сервер и раздача статики

--- springBoot/src/main/java/com/rog/teach/controller/MainController.java
+++ springBoot/src/main/java/com/rog/teach/controller/MainController.java
@@ -4,15 +4,24 @@ import com.rog.teach.domain.Message;
 import com.rog.teach.domain.User;
 import com.rog.teach.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
 
 import static com.rog.teach.utils.DateUtils.ZONE.ZONED_DATE_TIME_EUROPE_MOSCOW;
 
@@ -22,6 +31,12 @@ public class MainController {
 
     @Autowired
     private MessageRepo messageRepo;
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model){
@@ -44,8 +59,36 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@AuthenticationPrincipal User user, @RequestParam String text, @RequestParam String tag, Map<String, Object> model){
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model,
+            @RequestParam("file") MultipartFile file
+            ) throws IOException {
         Message message = new Message(text, tag, ZonedDateTime.now(ZONED_DATE_TIME_EUROPE_MOSCOW.getZone()).toLocalDateTime(), user);
+
+        if(file != null && !Objects.requireNonNull(file.getOriginalFilename()).isEmpty() && file.getSize()/1024 < 50){
+            File uploadDir = new File(System.getProperty("java.io.tmpdir") +uploadPath );
+            if(!uploadDir.exists()){
+                uploadDir.mkdirs();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file.getOriginalFilename();
+
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(System.getProperty("java.io.tmpdir") +uploadPath  + "/" + resultFileName)));
+                stream.write(bytes);
+                stream.close();
+            } catch (Exception e) {
+                System.out.println("sdf");
+            }
+
+            message.setFilename(resultFileName);
+        }
+
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
