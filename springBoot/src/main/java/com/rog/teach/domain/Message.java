43b026bc3206bbc805d37449da767a3e6dc1745f
GeneralRogov <alexanderogov@list.ru>
2021-05-09 23:21:34 +0300

    Добавил миграцию и страницу профиля.

--- springBoot/src/main/java/com/rog/teach/domain/Message.java
+++ springBoot/src/main/java/com/rog/teach/domain/Message.java
@@ -16,7 +16,7 @@ import static com.rog.teach.utils.DateUtils.getStrDate;
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String text;
     private String tag;
---
+++ springBoot/src/main/resources/db/migration/V1__Init_DB.sql
@@ -0,0 +1,35 @@
+create sequence if not exists hibernate_sequence start 1 increment 1;
+
+create table if not exists message_$t (
+    id int8 not null,
+    date_message
+    timestamp,
+    file varchar(65535),
+    tag varchar(255),
+    text varchar(2048) not null,
+    user_id int8,
+     primary key (id)
+ );
+
+create table if not exists user_$t (
+    id int8 not null,
+    activation_code varchar(255),
+    active boolean not null,
+    email varchar(255),
+    password varchar(255) not null,
+    username varchar(255) not null,
+    primary key (id)
+);
+
+create table if not exists userrole_$t (
+    user_id int8 not null,
+    roles varchar(255)
+);
+
+alter table if exists message_$t
+    add constraint message_user_fk
+    foreign key (user_id) references user_$t;
+
+alter table if exists userrole_$t
+    add constraint user_role_user_fk
+    foreign key (user_id) references user_$t;
