09d1f6254c082d629936fd01681ef4f03564cf08
GeneralRogov <alexanderogov@list.ru>
2021-05-11 00:04:52 +0300

    Исправление миграции

--- springBoot/src/main/resources/db/migration/V2__Add_admin.sql
+++ springBoot/src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,5 +1,5 @@
-insert into user_$t (id, username, password, active)
-    values (10000, 'rogov', 'qwer', true);
+insert into user_$t (id, username, password, activation_code, active)
+    values (10000, 'rogov', 'qwer', 'null', true);
 
 insert into userrole_$t (user_id, roles)
     values (10000, 'USER'), (10000, 'ADMIN');
