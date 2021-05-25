c229018404ed61529e1959a661999ac3738aad3b
GeneralRogov <alexanderogov@list.ru>
2021-05-26 00:51:59 +0300

    Добавил Turbolinks

---
+++ springBoot/src/main/java/com/rog/teach/utils/RedirectInterceptor.java
@@ -0,0 +1,18 @@
+package com.rog.teach.utils;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RedirectInterceptor implements HandlerInterceptor {
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        if(modelAndView != null){
+            String args = request.getQueryString() != null ? request.getQueryString() : "";
+            String url = request.getRequestURI().toString() + "?" + args;
+            response.setHeader("Turbolinks-Location", url);
+        }
+    }
+}
