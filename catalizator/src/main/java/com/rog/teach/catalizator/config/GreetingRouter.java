ed7b7fa9476fa5e3cdf4f1aab6856c64ebd376a0
GeneralRogov <alexanderogov@list.ru>
2021-05-27 23:06:57 +0300

    WebFlux. Реактивный web с Spring

---
+++ catalizator/src/main/java/com/rog/teach/catalizator/config/GreetingRouter.java
@@ -0,0 +1,24 @@
+package com.rog.teach.catalizator.config;
+
+import com.rog.teach.catalizator.handlers.GreetingHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.*;
+
+@Configuration
+public class GreetingRouter {
+    @Bean
+    public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
+        RequestPredicate route = RequestPredicates
+                .GET("/hello")
+                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN));
+
+        return RouterFunctions
+                .route(route, greetingHandler::hello)
+                .andRoute(
+                        RequestPredicates.GET("/"),
+                        greetingHandler::index
+                );
+    }
+}
---
+++ catalizator/src/test/java/com/rog/teach/catalizator/CatalizatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rog.teach.catalizator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalizatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
