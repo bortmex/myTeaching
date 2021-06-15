1a1a4c366cda8b89c3fd93dff12fa804c685c3f4
GeneralRogov <alexanderogov@list.ru>
2021-06-15 23:38:38 +0300

    Simple Http Server

--- javaCore/src/main/java/com/rog/teach/httpExample/httpServer/Main.java
+++ javaCore/src/main/java/com/rog/teach/httpExample/httpServer/Main.java
@@ -2,6 +2,8 @@ package com.rog.teach.httpExample.httpServer;
 
 public class Main {
     public static void main(String[] args) {
-        new Server().bootstrap();
+        new Server((req, resp) ->{
+            return "<html><body><h1>Hello, naive</h1>It amazing789</body></html>";
+        }).bootstrap();
     }
 }
