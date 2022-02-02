8018cb8a5d3f30ba272f0b06016e247f52a8a4b6
Alex <alexanderogov@gmail.com>
2022-02-02 23:31:45 +0300

    Удаление проекта

--- exampleTrackerApi/README.md
+++
@@ -1,2 +0,0 @@
-Code get with resources:
-https://www.youtube.com/watch?v=gcT5RPfa10U&t=2523s&ab_channel=MayCode
--- exampleTrackerApi/src/main/java/com/rog/teach/repositories/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rog.teach.repositories;
-
-import com.rog.teach.entity.TaskEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
-}
